b0f8d5aaaa3f503d6416217c5941a1696c5dad3f
michaelfarias <michaelfarias01@gmail.com>
2021-02-25 16:48:50 -0300

    quarta aula finalizada - notificação

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,12 @@
+import { useContext } from 'react';
+
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/michaelfarias.png" alt="Michael" />
@@ -8,8 +14,8 @@ export function Profile() {
                 <strong>Michael Farias</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
-                    </p>
+                    Level {level}
+                </p>
             </div>
         </div>
     );
